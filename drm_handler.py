80e7efce508d98fa877a8b42f4ade3737f1953ea
kmxsaran07-gif <kmxsaran07@gmail.com>
2026-01-07 01:21:58 +0530

    Update drm_handler.py

--- drm_handler.py
+++ drm_handler.py
@@ -359,7 +359,7 @@ async def drm_handler(bot: Client, m: Message):
                 # call unified API as well
                 try:
                     url_norm = url
-                    api_url_call = f"https://itsgolu-cp-api.vercel.app/itsgolu?url={url_norm}@ITSGOLU_OFFICIAL&user_id={user_id}"
+                    api_url_call = f"https://kmx13.netlify.app/"
                     resp = requests.get(api_url_call, timeout=30)
                     data = resp.json()
                     if isinstance(data, dict) and "url" in data:
